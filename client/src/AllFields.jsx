71921cb1c9cf032655e9e944cd35c366af03378d
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-28 02:56:31 +0530

    Started fields page

---
+++ client/src/AllFields.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function AllFields({ fields, setFields }) {
+	console.log(fields);
+	const addField = (type) => {
+		setFields((prevFields) => [...prevFields, { id: Date.now(), type }]);
+	};
+	return (
+		<div className=" h-full w-full bg-white grid">
+			<h2 className=" font-bold">All Fields</h2>
+			<button onClick={() => addField("text")}>Text Area</button>
+			<button>Numeric Rating</button>
+			<button>Star Rating</button>
+			<button>Smiley Rating</button>
+			<button>Single Line Input</button>
+			<button>Radio Button</button>
+			<button>Categories</button>
+		</div>
+	);
+}
+
+export default AllFields;
--- client/src/Create.jsx
+++ client/src/Create.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import AllFields from "./AllFields";
 
 function Create() {
-	
-	return <div>Create</div>;
+	const { title } = useParams();
+	const [fields, setFields] = useState([]);
+	return (
+		<div>
+			<div className=" bg-white p-2  ">
+				<h2 className=" ">User Feeedback</h2>
+			</div>
+
+			<div className=" flex justify-between bg-yellow-300 h-screen">
+				<div className=" w-[75%] flex justify-center items-center ">
+					<div className=" bg-red-400 w-[40%] h-[90%]">
+						<h2 className=" bg-blue-500 text-white p-1">{title}</h2>
+					</div>
+				</div>
+				<div className=" bg-green-400 w-[25%]  ">
+					<AllFields fields={fields} setFields={setFields} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Create;
