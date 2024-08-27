f70ff8d227a13d60a6b8c582cb5286a5645fedb5
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-27 14:35:49 +0530

    Configured firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
+import DashBoard from "./DashBoard";
+import "./firebase";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 function App() {
 	const [count, setCount] = useState(0);
-
+	const db = getFirestore();
+	const save = async () => {
+		try{
+		console.log("Hi");
+		await addDoc(collection(db, "myCollection"), {
+			field1: count,
+		});
+		console.log("Hi");
+		alert("Document Written");}
+		catch(error){
+			console.log(error)
+		}
+	};
 	return (
 		<>
-			<div className=" bg-green-400">Hi</div>
+			<div className=" bg-green-400">
+				<div>
+					<button className=" bg-yellow-300" onClick={() => save()}>
+						Hi
+					</button>
+				</div>
+				<DashBoard />
+			</div>
 		</>
 	);
 }
